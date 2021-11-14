35aedb692e23502bcbe133a59dafef0d43a67b40
amsriniva2001 <59710877+amsriniva2001@users.noreply.github.com>
2021-11-13 21:19:04 -0500

    push

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -68,8 +68,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   let object2 = removeKey(object, key);
-   return object2;
+   delete object[key];
 }
 
 /**
