60447346a3670d2a9febbc51eb8d5321c727aa22
amsriniva2001 <59710877+amsriniva2001@users.noreply.github.com>
2021-11-13 19:01:23 -0500

    push

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -48,7 +48,8 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-
+   let pos = object.indexOf(key);
+   object.splice(pos, 1);
 }
 
 /**
