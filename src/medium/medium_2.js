c2b05b08009418c8da7a411a43c1d9920218c869
amsriniva2001 <59710877+amsriniva2001@users.noreply.github.com>
2021-11-27 22:03:04 -0500

    push

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -91,13 +91,13 @@ export const allCarStats = {
  let hybridArray = mpg_data.filter(car => car.hybrid == true);
  let newArray = hybridArray.map(car => {return { make: car.make, id: car.id }});
  let hArray = [];
- let finalArray = hybridArray.reduce(function(previousValue, currentValue) {
+ let finalArray = map(hybridArray, reduce(function(previousValue, currentValue) {
    if(previousValue.make != currentValue.make) {
       hArray = [];
     }
     hArray.push(currentValue.id);
     return {"make": currentValue.make, "hybrids": hArray}
- });
+ }));
 
 /*
  function groupBy(objectArray, property) {
